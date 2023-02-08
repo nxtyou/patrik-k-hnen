da4e5a37dc27f7ecc12358a22d70617a4e64fb59
yamanidev <elamine.yamani@gmail.com>
2023-02-08 20:33:25 +0100

    Footer added

--- pages/bmw-open.jsx
+++ pages/bmw-open.jsx
@@ -3,6 +3,7 @@ import Analysis from '@/components/Templates/BMWOpen/Analysis';
 import Banner from '@/components/Templates/BMWOpen/Banner';
 import Gallery from '@/components/Templates/BMWOpen/Gallery';
 import Hero from '@/components/Templates/BMWOpen/Hero';
+import Footer from '@/components/Templates/Shared/Footer/Footer';
 import NavBar2 from '@/components/Templates/Shared/NavBar/NavBar2';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -17,6 +18,7 @@ function BMWOpen() {
         <Gallery />
         <About />
         <Banner />
+        <Footer />
       </main>
     </>
   );
