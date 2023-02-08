5ae3929b097772d0d0b2fe78c33ec4e22c0b20f2
yamanidev <elamine.yamani@gmail.com>
2023-02-08 18:45:05 +0100

    Removed space from link

--- components/Templates/SkyTennis/Analysis.jsx
+++ components/Templates/SkyTennis/Analysis.jsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 const Analysis = () => {
   return (
-    <section id='more' className="py-16 px-6 sm:px-8 md:px-20 xl:px-32">
-      <div className="mx-auto flex flex-col-reverse lg:flex-row max-w-7xl ">
-        <div className='mt-10 lg:mt-0'>
+    <section id="more" className="py-16 px-6 sm:px-8 md:px-20 xl:px-32">
+      <div className="mx-auto flex max-w-7xl flex-col-reverse lg:flex-row ">
+        <div className="mt-10 lg:mt-0">
           <h2 className="text-sm font-semibold">DIENSTLEISTUNGEN</h2>
           <ul className="mt-3 text-lg leading-9 text-white/80">
             <li>Moderator & Speaker</li>
@@ -13,13 +13,17 @@ const Analysis = () => {
             <li>Medienstrategie</li>
           </ul>
           <h2 className="mt-12 text-sm font-semibold">MEHR INFORMATIONEN</h2>
-          <Link className="mt-3 text-lg leading-9 link-underline relative" href="http://www.eineadresse.de/">
-            www.eineadresse.de&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link
+            className="link-underline relative mt-3 text-lg leading-9"
+            href="http://www.eineadresse.de/">
+            www.eineadresse.de
           </Link>
         </div>
         <div className=" lg:pl-32">
-          <h2 className="text-2xl md:text-3xl font-medium">Wimbledon Analysen und Vertrauen von SKY</h2>
-          <div className="mt-6 md:mt-7 md:text-lg lg:text-xl text-white/80">
+          <h2 className="text-2xl font-medium md:text-3xl">
+            Wimbledon Analysen und Vertrauen von SKY
+          </h2>
+          <div className="mt-6 text-white/80 md:mt-7 md:text-lg lg:text-xl">
             <p className="lg:!leading-loose">
               Schon in seiner Jugend war Patrik von diesem Turnier fasziniert und inspiriert. Das
               Erreichen des Viertelfinales 1988 und der Sieg gegen Jimmy Connors in fünf
