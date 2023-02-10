d86112a12b7b24d1b6cfc2fb9cc7769413bac5c9
yamanidev <elamine.yamani@gmail.com>
2023-02-10 21:20:51 +0100

    keynote-speaker Gallery made responsive

--- components/Templates/KeynoteSpeaker/Gallery.jsx
+++ components/Templates/KeynoteSpeaker/Gallery.jsx
@@ -4,18 +4,18 @@ const Gallery = () => {
       <div className="mx-auto max-w-7xl">
         <div className="relative">
           <img
-            className="w-full object-cover"
+            className="min-h-[205px] w-full object-cover"
             src="/images/photos/patrik-as-speaker2.jpg"
             width="1216"
             height="684"
             alt=""
           />
         </div>
-        <div className="mt-3 flex max-h-[335px] flex-col md:mt-6 md:flex-row md:space-x-6">
+        <div className="mt-3 flex flex-col md:mt-6 md:flex-row md:space-x-6">
           <div className="md:w-1/2">
             <img
               style={{ objectPosition: '50% 20%' }}
-              className="h-full w-full object-cover"
+              className="max-h-[335px] w-full object-cover"
               width="596"
               height="335"
               alt=""
@@ -25,7 +25,7 @@ const Gallery = () => {
           <div className="mt-3 md:mt-0 md:w-1/2">
             <img
               style={{ objectPosition: '50% 25%' }}
-              className="h-full w-full object-cover"
+              className="max-h-[335px] w-full object-cover"
               width="596"
               height="335"
               alt=""
@@ -33,11 +33,11 @@ const Gallery = () => {
             />
           </div>
         </div>
-        <div className="mt-3 flex max-h-[596px] gap-3 md:mt-6 md:gap-6">
+        <div className="mt-3 flex gap-3 md:mt-6 md:gap-6">
           <div className="flex-1">
             <img
               style={{ objectPosition: '50% 10%' }}
-              className="h-full w-full object-cover object-top md:aspect-auto md:object-center"
+              className="max-h-[596px] w-full object-cover object-top md:aspect-auto md:object-center"
               width="373"
               height="596"
               alt=""
@@ -47,7 +47,7 @@ const Gallery = () => {
           <div className="flex-1">
             <img
               style={{ objectPosition: '50% 10%' }}
-              className="h-full w-full object-cover object-right md:aspect-auto"
+              className="max-h-[596px] w-full object-cover object-right md:aspect-auto"
               width="596"
               height="596"
               alt=""
