6a42cb88db66989cf5564263824e695d89efa1d6
yamanidev <elamine.yamani@gmail.com>
2023-02-10 18:09:23 +0100

    Changed navbar for homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,18 @@
+import About from '@/components/Templates/Homepage/About';
 import Awards from '@/components/Templates/Homepage/Awards';
-import References from '@/components/Templates/Homepage/References';
 import Clients from '@/components/Templates/Homepage/Clients';
-import Footer from '@/components/Templates/Shared/Footer/Footer';
 import Hero from '@/components/Templates/Homepage/Hero';
-import NavBar from '@/components/Templates/Shared/NavBar/NavBar';
 import Philosophie from '@/components/Templates/Homepage/Philosophie';
+import References from '@/components/Templates/Homepage/References';
+import Footer from '@/components/Templates/Shared/Footer/Footer';
+import NavBar2 from '@/components/Templates/Shared/NavBar/NavBar2';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import About from '@/components/Templates/Homepage/About';
 
 export default function Home() {
   return (
     <>
-      <NavBar />
+      <NavBar2 />
       <main>
         <Hero />
         <About />
