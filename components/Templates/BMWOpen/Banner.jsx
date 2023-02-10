d6d42aafb85a23037f31f459505a17bc1b06c2a1
yamanidev <elamine.yamani@gmail.com>
2023-02-10 19:50:14 +0100

    Changed Banner of bmw-open

--- components/Templates/BMWOpen/Banner.jsx
+++ components/Templates/BMWOpen/Banner.jsx
@@ -1,4 +1,5 @@
 import Button from '@/components/Atoms/Button';
+import ButtonLink from '@/components/Atoms/ButtonLink';
 import ArrowRight2 from '@/components/Atoms/Icons/ArrowRight2';
 
 const Banner = () => {
@@ -11,7 +12,7 @@ const Banner = () => {
       className="relative py-24 px-6 sm:px-8 md:py-32 md:px-20 xl:px-32">
       <img
         className="absolute top-0 left-0 -z-10 h-full w-full object-cover"
-        src="/images/photos/patrik-as-speaker.jpg"
+        src="/images/photos/patrik-playing-tennis.jpg"
         width="1440"
         height="613"
         alt=""
@@ -20,22 +21,23 @@ const Banner = () => {
         <div
           style={{ background: 'rgba(81, 81, 81, 0.2)' }}
           className="w-fit rounded-[0.2rem] py-1.5 px-3 text-sm font-semibold uppercase tracking-widest">
-          SPEAKER
+          COACH
         </div>
         <img
           className="mt-16 md:mt-40"
-          src="/images/logo.svg"
+          src="/images/logos/robinson.svg"
           width="198"
           height="22"
-          alt="Patrik Kühnen logo"
+          alt="robinson logo"
         />
-        <h2 className="mt-9 text-5xl font-medium md:text-6xl">Keynote Speaker</h2>
-        <Button
+        <h2 className="mt-9 text-5xl font-medium md:text-6xl">Top Event Coach</h2>
+        <ButtonLink
+          href="/top-event-coach"
           aria-label="Mehr Informationen"
           className="group mt-10"
           rightIcon={<ArrowRight2 animated black />}>
           Mehr Informationen
-        </Button>
+        </ButtonLink>
       </div>
     </section>
   );
