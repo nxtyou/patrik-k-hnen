340eacd6feca76d512c835f24c28aa52513ca7a5
yamanidev <elamine.yamani@gmail.com>
2023-02-23 16:19:50 +0100

    Added a second arrow down icon

---
+++ components/Atoms/Icons/ArrowDown2.jsx
@@ -0,0 +1,19 @@
+function ArrowDown2({ className }) {
+  return (
+    <svg
+      width="18"
+      height="10"
+      className={className}
+      viewBox="0 0 18 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M16.1602 1.27344L8.91016 8.52344L1.66016 1.27344"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
+export default ArrowDown2;
