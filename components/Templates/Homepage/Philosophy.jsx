49bc30b747047dae0ca6b29bb925f887dd88af22
yamanidev <elamine.yamani@gmail.com>
2023-02-24 10:21:14 +0100

    Increased text size

--- components/Templates/Homepage/Philosophy.jsx
+++ components/Templates/Homepage/Philosophy.jsx
@@ -6,7 +6,7 @@ function Philosophy() {
       <div className="mx-auto flex h-[600px] w-full max-w-7xl items-center px-6 sm:px-8 md:px-20 2xl:px-0">
         <div className="max-w-lg">
           <h2 className="text-[3.5rem] font-medium">Philosophie</h2>
-          <p className="mt-3 mb-10 text-[#fff]">
+          <p className="mt-3 mb-10 text-xl text-[#fff]">
             Langfristige Zusammenarbeit ist Patrik Kühnen wichtig. Deshalb trifft er Entscheidungen
             für Partnerschaften stets bewusst und auf Basis gemeinsamer Werte.{' '}
           </p>
